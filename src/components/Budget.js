78cdae1a5c4937e8827ccb1e09aee1b69e12e5f1
Diamond Diaso <diasodiamond@gmail.com>
2023-03-14 18:01:29 +0000

    Final Project for React-Redux
    Signed-off-by: Diamond Diaso <diasodiamond@gmail.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,22 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  const { budget } = useContext(AppContext);
+  return (
+    <div className="alert alert-secondary">
+      <span>
+        Budget: £
+        <input
+          required="required"
+          type="number"
+          id="budget"
+          value={budget}
+          style={{ size: 10 }}
+          step={10}
+          max={20000}
+        ></input>
+      </span>
+    </div>
+  );
 };
 export default Budget;
